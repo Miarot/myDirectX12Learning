57548ed1c5e13efe5bc9dab7c4971508c5aaf30e
Miarot <zinchenkodan@gmail.com>
2023-04-08 22:09:46 +0300

    make basic project build

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Windows.h>
+#include <iostream>
+#include <d3d12.h>
+
+int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow)
+{
+	int a = 10;
+	int b = 20;
+
+	std::cout << "Hello DirectX 12" << std::endl;
+	system("pause");
+
+	return 0;
+}
